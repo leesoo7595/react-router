7c43f4a4adbde835e241a8fc74b003ba5c29db03
leesoo <isoo7510@gmail.com>
2019-02-19 11:41:33 +0900

    router parameter exact, params

--- src/shared/App.js
+++ src/shared/App.js
@@ -7,7 +7,8 @@ class App extends Component {
         return (
           <div>
               <Route exact path="/" component={Home}/>
-              <Route path="/about" component={About}/>
+              <Route exact path="/about" component={About}/>
+              <Route path="/about/:name" component={About}/>
           </div>
         );
     }

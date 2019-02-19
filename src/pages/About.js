2c49446389fd08167235005d122510450aa18bdf
leesoo <isoo7510@gmail.com>
2019-02-19 12:01:21 +0900

    router parameter query string

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import queryString from 'query-string';
 
-const About = ({match}) => {
-  return (
+const About = ({location, match}) => {
+    const query = queryString.parse(location.search);
+    // params 끝에 ?detail=true가 붙는 것
+    const detail = query.detail === 'true';
+
+    return (
     <div>
         <h2>About {match.params.name}</h2>
+        {detail && 'detail: blahblah'}
     </div>
   );
 };
